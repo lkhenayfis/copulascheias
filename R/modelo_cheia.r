9e8448634995a4ec51c8b4f2f933f5453d28b3e9
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-09-24 16:19:46 -0300

    Melhora doc e reformula interface de modelo_cheias

--- R/modelo_cheia.r
+++ R/modelo_cheia.r
@@ -2,32 +2,35 @@
 #' Ajuste De Modelo De Cheias
 #' 
 #' Estima uma vine copula considerando todos os aspectos de cheia em `data`
-#' @param data `data.table` de dados de cheias
-#' @param postos vetor de postos a serem modelados
-#' @param anos vetor indicando quais anos devem ser considerados para o modelo; `NULL` usa todos os
-#'     anos disponiveis em `data`
+#' 
+#' O argumento `data` deve ser um `data.table`-like em que cada coluna corresponde a uma variavel
+#' da distribuicao multivariada a ser estimada. Os nomes das colunas sao de grande importancia neste
+#' pacote pois serao posteriormente utilizadas para interpretacao dos eventos de inferencia (veja
+#' [pcopula()]).
+#' 
+#' @param data `data.table` de dados em que cada coluna representa uma variavel. Veja Detalhes
 #' @param vine_params lista de argumentos para estimacao da vine copula, pode ser composta de
 #'     qualquer argumento da funcao [VineCopula::RVineStructureSelect()]
 #' 
-#' @return objeto `modelo_cheias`
+#' @return objeto `modelo_cheias`, uma lista de apenas um elemento `"vines"` contendo o modelo de
+#'     `VineCopula` estimado
+#' 
+#' @examples
+#' 
+#' # utilizando parte do dado interno do pacote
+#' data <- minicheias[, 1:3]
+#' mod <- fit_modelo_cheias(data)
 #' 
 #' @export
 
-fit_modelo_cheia <- function(data, postos, anos = NULL, vine_params = default_vine_params()) {
-    data <- data[posto %in% postos]
-    if (!is.null(anos)) data <- data[ano %in% anos]
-
-    data <- melt(data, id.vars = c("posto", "ano"))
-    data <- dcast(data, ano ~ posto + variable)[, -1]
-
+fit_modelo_cheia <- function(data, vine_params = default_vine_params()) {
     ecdfs <- lapply(data, ecdf)
-
     data_u <- mapply(function(v, f) f(v), data, ecdfs)
 
     cc <- as.call(c(list(quote(RVineStructureSelect), data = quote(data_u)), vine_params))
     vines <- eval(cc)
 
-    new_modelo_cheia(vines, ecdfs, postos, anos)
+    new_modelo_cheia(vines, ecdfs)
 }
 
 default_vine_params <- function() {
@@ -35,13 +38,8 @@ default_vine_params <- function() {
 }
 
 new_modelo_cheia <- function(vines, ecdfs, postos, anos) {
-    num_postos <- length(postos)
-    num_anos   <- length(anos)
-
     new <- structure(list(vines = vines), class = c("modelo_cheia", "list"))
     attr(new, "ecdfs")  <- ecdfs
-    attr(new, "postos") <- postos
-    attr(new, "anos") <- anos
 
     return(new)
 }
