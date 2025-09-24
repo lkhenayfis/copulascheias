efb5777b8087bcc30c05ad0f2aa9beabbefb58ba
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-09-24 18:27:55 -0300

    Add testes de inferencia

---
+++ tests/testthat/test-inference.r
@@ -0,0 +1,41 @@
+test_that("parse_inference", {
+    modelo <- fit_modelo_cheia(minicheias)
+    expr <- "(ernestina_volume >= 400) & (ernestina_pico >= 100)"
+
+    inference <- parse_inference(expr, modelo)
+    expect_true(inherits(inference, "simple_inference"))
+
+    outer <- list(
+        parse_unitary_expr("ernestina_volume >= 400", modelo),
+        parse_unitary_expr("ernestina_pico >= 100", modelo)
+    )
+    expect_equal(unclass(inference), outer)
+
+    expr <- "(ernestina_volume >= 400) | (ernestina_pico >= 100)"
+    expect_error(parse_inference(expr, modelo))
+})
+
+test_that("simple_inference2bounds", {
+    modelo <- fit_modelo_cheia(minicheias)
+    inference <- parse_inference("(ernestina_volume >= 200) & (ernestina_pico >= 100)", modelo)
+
+    bounds <- simple_inference2bounds(inference)
+    expect_equal(
+        round(bounds[[1]], 7),
+        structure(c(0.9397590, 0.1204819), names = c("ernestina_volume", "ernestina_pico"))
+    )
+    expect_equal(
+        round(bounds[[2]], 7),
+        structure(c(1, 1), names = c("ernestina_volume", "ernestina_pico"))
+    )
+
+    bounds <- simple_inference2bounds(inference, modelo)
+    expect_equal(
+        round(bounds[[1]], 7),
+        structure(c(0, 0, 0, 0.1204819, 0, 0.9397590), names = modelo$vines$names)
+    )
+    expect_equal(
+        round(bounds[[2]], 7),
+        structure(rep(1, 6), names = modelo$vines$names)
+    )
+})
