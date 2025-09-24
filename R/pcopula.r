e7d964af99de6969556d04984ecb248202fd29c8
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-09-24 16:29:10 -0300

    Melhora doc de pcopula e add exemplo

--- R/pcopula.r
+++ R/pcopula.r
@@ -2,8 +2,33 @@
 #' 
 #' Calcula a probabilidade de um evento `expr` de acordo com a copula estimada `modelo`
 #' 
-#' @param expr string descrevendo o evento cuja probabilidade deve ser calculada
+#' `expr` e o principal argumento desta funcao. Atraves dele sao informadas expressoes simbolicas
+#' que qualificam um evento cuja probabilidade sera calculada. Ha duas partes destas expressoes.
+#' 
+#' Primeiro, devem ser descritos os eventos unitarios. Estes sao da forma `"var <= b"`, em que
+#' `"var"` e uma das variaveis representadas em `modelo` e `b` um valor de bound. Sao suportadas
+#' expressoes das formas `"var <= b"`, `"var >= b"` e `"a <= var <= b"`.
+#' 
+#' Eventos unitarios podem ser combinados por intersecao, utilizando os simbolos `&` e `|`
+#' respectivamente. Por exemplo, uma expressao de dois eventos seria "(var1 <= b1) & (var2 >= b2)".
+#' Nao ha qualquer limite no numero de eventos.
+#' 
+#' @param expr string descrevendo o evento cuja probabilidade deve ser calculada. Veja Detalhes
 #' @param modelo modelo de copula como retornado por [`fit_modelo_cheia()`]
+#' 
+#' @return escalar indicando a probabilidade do evento `expr`
+#' 
+#' @examples 
+#' 
+#' # utilizando parte do dado interno do pacote
+#' data <- minicheias[, 1:3]
+#' mod <- fit_modelo_cheias(data)
+#' 
+#' # evento simples, considerando apenas uma variavel
+#' pcopula("ernestina >= 500", modelo)
+#' 
+#' # considerando agora duas
+#' pcopula("(ernestina_pico >= 500) & (ernestina_volume >= 150)", modelo)
 #' 
 #' @export
 
@@ -21,6 +46,8 @@ pcopula <- function(expr, modelo) {
 #' @param inference objeto inferenca
 
 run_inference <- function(inference, modelo) UseMethod("run_inference", inference)
+
+#' @rdname run_inference
 
 run_inference.simple_inference <- function(inference, modelo, nsims = 2e6, threads = 1) {
     bounds <- simple_inference2bounds(inference, modelo)
